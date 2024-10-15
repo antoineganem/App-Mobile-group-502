411ab03fa90b70298c7c650b22d276bf14a864e3
Santi Gutierrez Gonzalez <santigutierrezgonzalez@Santis-MacBook-Air.local>
2024-10-14 20:20:45 -0500

    Repo creation

---
+++ back-tomate/src/index.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
