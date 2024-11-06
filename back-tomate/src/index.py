676254bdb86e6e3efe3654fdc6496a2fa5c54759
santigugon <santigugon@gmail.com>
2024-11-06 11:22:40 -0600

    donation get endpoint working

--- back-tomate/src/index.py
+++ back-tomate/src/index.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flaskr.db import get_db, close_db
 from .activities.routes import activities_bp
 from .hours.routes import hours_bp
+from .donations.routes import donations_bp
 
 app = Flask(__name__)
 
@@ -29,6 +30,7 @@ def get_users():
 
 app.register_blueprint(activities_bp)
 app.register_blueprint(hours_bp)
+app.register_blueprint(donations_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
